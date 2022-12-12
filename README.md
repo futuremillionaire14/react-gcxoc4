f301063ac1a2d719418db3548c939c5a4970592c
futuremillionaire14 <120112238+futuremillionaire14@users.noreply.github.com>
2022-12-12 14:35:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-gcxoc4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gcxoc4)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+import React from 'react'
+import { render } from 'react-dom'
+window.React = React
+
+function App() {
+  return <Navbar />;
+}
+
+export default App
